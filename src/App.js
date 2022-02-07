65347531b47abd33878ce9732aa527d0848b041a
Matti Luukkainen <mluukkai@iki.fi>
2022-02-07 23:40:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,14 +90,14 @@ const App = () => {
       author: 'Jez Humble',
       info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
       votes: 0,
-      id: '1'
+      id: 1
     },
     {
       content: 'Premature optimization is the root of all evil',
       author: 'Donald Knuth',
       info: 'http://wiki.c2.com/?PrematureOptimization',
       votes: 0,
-      id: '2'
+      id: 2
     }
   ])
 
